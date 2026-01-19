41123d654f6fe8c68b039f172e216c67c410178e
Candra <84482710+Candra2222@users.noreply.github.com>
2026-01-19 19:58:28 +0700

    Update deploy-vercel.js

--- api/deploy-vercel.js
+++ api/deploy-vercel.js
@@ -4,26 +4,16 @@ export default async function handler(req, res) {
   }
 
   const { repo } = req.body;
+  const token = process.env.VERCEL_TOKEN;
 
-  if (!repo) {
-    return res.status(400).json({ error: 'Repository tidak dikirim' });
+  if (!repo || !token) {
+    return res.status(400).json({ error: 'Missing data' });
   }
 
-  const token = process.env.VERCE_TOKEN;
-  const domain = process.env.SHORT_DOMAIN;
-
-  if (!token || !domain) {
-    return res.status(500).json({ error: 'ENV_NOT_SET' });
-  }
-
-  // repo: username/reponame
   const [owner, name] = repo.split('/');
   const projectName = `${owner}-${name}`.toLowerCase();
 
-  // slug random
-  const slug = Math.random().toString(36).substring(2, 8);
-
-  const response = await fetch('https://api.vercel.com/v13/deployments', {
+  const r = await fetch('https://api.vercel.com/v13/deployments', {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${token}`,
@@ -39,17 +29,11 @@ export default async function handler(req, res) {
     })
   });
 
-  const data = await response.json();
+  const data = await r.json();
 
-  if (!response.ok) {
-    return res.status(500).json({
-      error: 'DEPLOY_FAILED',
-      detail: data
-    });
-  }
+  if (!r.ok) return res.status(500).json(data);
 
   res.json({
-    short: `https://${domain}/${slug}`,
-    target: `https://${data.url}`
+    url: `https://${data.url}`
   });
 }
